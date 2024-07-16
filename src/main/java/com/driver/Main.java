6d122d2277f3d51eff4453bceb8fd18b6fffe3db
Prasad <psa19974@gmail.com>
2024-07-16 22:45:36 +0530

    Push

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int product(int x, int y) {return x*y;}
+
+        public int product(int x, int y, int z) {return x*y*z;}
+
+        public double product(double x, double y) {return x*y;}
+
+public static void main (String[] args){
+            Product p=new Product();
+
+            int a1=p.product(2,3);
+
+            int a2=p.product(2,3,4);
+            
+            double a3=p.product(2.3,3.4);
+
+}
+    }
 }
